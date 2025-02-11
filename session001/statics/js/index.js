f4aa6e02068a4e338f0f01baaafcad794c38b99b
Rychy248 <jorgeajrhh@gmail.com>
2025-02-10 22:09:48 -0600

    Ejercicio 4.1, organizar js

--- session001/statics/js/index.js
+++ session001/statics/js/index.js
@@ -1,5 +1,9 @@
 
 
 function imprimirNombre() {
+    
     let nombre = prompt("Dame tu nombre")
+    let imprimeMiNombre = document.getElementById("nombreUsuario")
+
+    imprimeMiNombre.innerHTML = `Bievenido ${nombre}`
 }
