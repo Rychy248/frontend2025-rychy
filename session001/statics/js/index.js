df5fe0c8cda7a93b8eef0e7d8b37d36cf1329b59
Rychy248 <jorgeajrhh@gmail.com>
2025-02-10 23:28:20 -0600

    ejercicios

--- session001/statics/js/index.js
+++ session001/statics/js/index.js
@@ -1,9 +1,49 @@
-
-
 function imprimirNombre() {
-    
-    let nombre = prompt("Dame tu nombre")
-    let imprimeMiNombre = document.getElementById("nombreUsuario")
+  let nombre = prompt("Dame tu nombre");
+  let imprimeMiNombre = document.getElementById("nombreUsuario");
 
-    imprimeMiNombre.innerHTML = `Bievenido ${nombre}`
+  imprimeMiNombre.innerHTML = `Bievenido ${nombre}`;
+}
+
+function concatenacionExtension() {
+  let nombreUsuario = prompt("Dame tu nombre");
+  // crea otra variable con un saludo
+
+  // En un alert imprime, el nombre, mas el saludo y el numero de caracteres del nombre
+
+  // .length para el nombre
+  // \n para un salto de linea
+  // concatena con + y con balstik
+}
+
+function getTheTime() {
+  // Piensa en 90 como la media de edad de un humano
+  /*
+    365 days in a year
+    52 weeks in a year
+    12 months in a year.
+    */
+  // humantext.innerHTML = "Tu tienes <b>" + x + " dias</b>, <b>" + y + " semanas</b>, and <b>" + z +  " meses</b> todavía.";
+}
+
+function calcularIBM() {
+  // obten los valores de peso y altura del select list
+  let weightType = document.getElementById("weight").value;
+  let heightType = document.getElementById("height").value;
+
+  // obtenen los valores de peso y altura de los inputs
+
+  // si es libras opera
+  // weight /= 2.20462;
+
+  // usa la formula
+  // BMI = (weight / heigth ^ 2)
+  // let BMI = weight /  height ** 2;
+
+  // evalua
+  // if BMI > 24.9 =overweight";
+  // if BMI < 24.9 && BMI >=18.5 = normal weight";
+  // if BMI under 18.5 = underweight"
+
+  // asigna a resultadoIBM
 }
