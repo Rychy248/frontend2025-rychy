afcd4d8687c0a0e6c4c8bbf2e7a6a0d45055247a
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-11 20:05:10 -0600

    udpate(functions) funcitions concepts

---
+++ session001/statics/js/conceptos.js
@@ -0,0 +1,26 @@
+
+
+
+/** FUNCIONES DECLARADAS */
+
+const suma = function(numero1, numero2) {
+    let resultado = numero1 + numero2
+    return resultado
+};
+
+function saludame1(){
+    alert("Hola")
+}
+
+function saludame2(nombre) {
+    let apellido = "cualquier apellido"
+    
+    alert(`Hola, bienvenido ${nombre} ${apellido}`)
+}
+
+
+
+function operacion() {
+
+    suma()
+}
