2b22578da867d5d374cc775ea00432eadedc56ee
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-18 18:16:54 -0600

    update(step1 ) fizz buzz

--- session002/static/js/index.js
+++ session002/static/js/index.js
@@ -8,14 +8,28 @@ function pintarEnPantalla() {
 }
 
 function esFizzOEsBuzz(num) {
-    // Encrgad de devolver, fizz, buzz o el numero
+    // Encargada de devolver, fizz, buzz o el numero
     // si es multiplo de 3, devolver fizz
     // si es multiplo de 5, devolver buzz
     // si es multiplo de 3 y 5, devolver fizzBuzz
+    if(num === 0){
+     return num
+    }else if(num % 3 === 0 && num % 5 === 0 ){
+        return 'fizzBuzz'
+    }else if(num % 3 === 0){
+        return 'fizz'
+    }else if(num % 5 === 0){
+        return 'buzz'
+    }
+    
+    return num
 }
 
 function siguiente(){
-    // añadir un siguiente numero a la array fizzBuzz
+    // añadir un siguiente numero aL array fizzBuzz
+    let siguienteElemento = esFizzOEsBuzz(++contador)
+    fizzBuzz.push(siguienteElemento)
+    console.log(fizzBuzz)
 }
 
 function siguienteN(rondas){
@@ -26,3 +40,8 @@ function limpiar(){
     //encargada de vaciar la lista fizzBuzz, y limpiar el paragrahp
 }
 // Hola estoy añadiendo una linea a este fichero
+
+// function main(){
+
+//     // ORQUESTAR LLAMADAS
+// }
