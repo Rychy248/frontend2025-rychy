329449da906efa5d866571ca22160f42a73a5b61
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-18 20:02:22 -0600

    update(fibonnaci) start

---
+++ session002/static/js/fibonnacci.js
@@ -0,0 +1,17 @@
+
+let ultimo = 1
+let penultimo = 1
+let siguienteNumero
+
+
+function fibonnacciSiguiente(){
+    // determinar el siguiente numero, y añadirlo al paragraph de los resultados de fibonacci
+}
+
+function fibonnacciSiguienteN(){
+    // iterar sobre la funcion siguiente
+}
+
+function fibonnacciLimpiar(){
+    // reiniciar las variables iniciales y luego limpiar el paragraph
+}
--- session002/static/js/index.js
+++ session002/static/js/index.js
@@ -3,15 +3,20 @@
 let contador = -1
 let fizzBuzz = []
 
+function cambiarParagraph(contenido='Fizz Buzz:'){
+    // recibir un parametro, string
+    // asignado al node con id 'fizzBuzzResult'
+    let nodeParrafo = document.getElementById('fizzBuzzResult');
+    nodeParrafo.innerHTML = contenido
+}
+
 function pintarEnPantalla() {
-    //Encargada de modificar el paragrahp "fizzBuzzResult"
+    cambiarParagraph(
+        'Fizz Buzz: ' + fizzBuzz.join(', ')
+    )
 }
 
 function esFizzOEsBuzz(num) {
-    // Encargada de devolver, fizz, buzz o el numero
-    // si es multiplo de 3, devolver fizz
-    // si es multiplo de 5, devolver buzz
-    // si es multiplo de 3 y 5, devolver fizzBuzz
     if(num === 0){
      return num
     }else if(num % 3 === 0 && num % 5 === 0 ){
@@ -26,22 +31,21 @@ function esFizzOEsBuzz(num) {
 }
 
 function siguiente(){
-    // añadir un siguiente numero aL array fizzBuzz
-    let siguienteElemento = esFizzOEsBuzz(++contador)
-    fizzBuzz.push(siguienteElemento)
-    console.log(fizzBuzz)
+    fizzBuzz.push(
+        esFizzOEsBuzz(++contador)
+    )
+    pintarEnPantalla()
 }
 
-function siguienteN(rondas){
-    // encargada de llamar de manera iterada, a la funcion siguiente
+function siguienteN(rondas=10){
+    for (let index = 0; index < rondas; index++) {
+        siguiente()
+    }
 }
 
 function limpiar(){
     //encargada de vaciar la lista fizzBuzz, y limpiar el paragrahp
+    fizzBuzz = []
+    contador = -1
+    cambiarParagraph()
 }
-// Hola estoy añadiendo una linea a este fichero
-
-// function main(){
-
-//     // ORQUESTAR LLAMADAS
-// }
