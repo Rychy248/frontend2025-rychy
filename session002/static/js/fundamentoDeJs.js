5d700c88c36f59c8f091b35bca55ed774cead234
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-24 20:56:25 -0600

    update(excercise) promise

---
+++ session002/static/js/fundamentoDeJs.js
@@ -0,0 +1,25 @@
+
+
+
+function renap(edad) {
+    // esperar un segundo, para decir si es mayor de edad
+
+    // si es mayor edad, les da un DPI
+
+    // si no es mayor de dad, los saca del RENAP, FUERA, reject
+
+
+    // return new PROMISE
+
+    // if, resolve : reject
+}
+
+
+function solicitarDPI() {
+    // PROMPT, pedir la edad
+    // si es mayor de edad. RESOLVE, felicitamos
+    // si no es mayor de edad. REJECT, "Lo sentimos"
+
+    // .then
+    // .catch
+}
