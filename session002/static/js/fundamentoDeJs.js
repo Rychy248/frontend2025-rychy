e51a01c7f3ffb8bcabc52a1f29e7e761518c74a1
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-24 21:22:55 -0600

    update(ejercicio1) promesas

--- session002/static/js/fundamentoDeJs.js
+++ session002/static/js/fundamentoDeJs.js
@@ -1,6 +1,9 @@
 
 
-
+/**
+ * 
+ * @param { number } edad 
+ */
 function renap(edad) {
     // esperar un segundo, para decir si es mayor de edad
 
@@ -10,13 +13,36 @@ function renap(edad) {
 
 
     // return new PROMISE
-
     // if, resolve : reject
+    return new Promise((resolver, rechazar)=>{
+        setTimeout(() => {
+            
+            if(edad >= 18){
+                resolver('Tenga su DPI')
+            }else{
+                rechazar("Fuera, usted es menor de edad.")
+            }
+
+        }, 1000);
+    })
 }
 
 
 function solicitarDPI() {
     // PROMPT, pedir la edad
+    const edad = Number(prompt("Por favor deme su edad"));
+
+    const siResuelve = (mensaje) =>{
+        alert(`FELICITACIONES: ${mensaje}`)
+    };
+
+    const siRechaza = (mensaje)=>{
+        alert(`LO SENTIMOS: ${mensaje}`)
+    };
+
+    renap(edad)
+    .then(siResuelve)
+    .catch(siRechaza)
     // si es mayor de edad. RESOLVE, felicitamos
     // si no es mayor de edad. REJECT, "Lo sentimos"
 
