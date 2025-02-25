68dea1de975c8806a1ec215834aafd8ebf2eec6c
Ricardo Hernández <jorgeajrhh@gmail.com>
2025-02-24 18:09:57 -0600

    update(operaciones) update code

---
+++ session002/static/js/operaciones.js
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * @param { number } num1 sumando1 de la adicion
+ * @param { number } num2 sumando2 de la adcion
+ * @returns 
+ */
+const suma = (num1, num2) =>{
+    return num1 + num2
+}
+
+
+/**
+ * 
+ * @param { string } operacionArealizar Esto define el tipo de operaecion a realizar
+ */
+function operacion(operacionArealizar) {
+    
+}
+
